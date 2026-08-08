87f3b638c69537b2daaa2973aafcf263e3ba52aa
meh <no-reply@pathscale.com>
2026-08-09 04:24:17 +0700

    Expose preview debug control

--- apps/blitz-preview/src/main.rs
+++ apps/blitz-preview/src/main.rs
@@ -1,6 +1,6 @@
 use anyrender_vello_cpu::VelloCpuWindowRenderer as WindowRenderer;
 use blitz_dom::DocumentConfig;
-use blitz_script::ScriptDocument;
+use blitz_script::{DebugController, ScriptDocument};
 use blitz_shell::{BlitzApplication, BlitzShellProxy, WindowConfig, create_default_event_loop};
 
 include!(concat!(env!("OUT_DIR"), "/embedded.rs"));
@@ -13,6 +13,11 @@ fn main() {
 
     let window = WindowConfig::new(Box::new(document), WindowRenderer::new());
     let mut application = BlitzApplication::new(proxy, receiver);
+    if let Some(controller) = DebugController::start_from_env(env!("CARGO_PKG_VERSION"))
+        .expect("invalid Blitz debug-control configuration")
+    {
+        application.set_debug_controller(controller);
+    }
     application.add_window(window);
     event_loop
         .run_app(application)
